5b1dfabc035304617fa122949c935b3059a9afb7
joshbrookstone <joshbrookstone@gmail.com>
2021-12-19 12:49:52 -0800

    changedtomulti

---
+++ src/Components/Title.jsx
@@ -0,0 +1,23 @@
+export default function Title() {
+  return (
+    <div className="flex-col mt-lg">
+      <span className="font-lg gray weight-300"></span>
+      <h2 className="m-0 white"></h2>
+      <h1
+        className=" white mb-md"
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <span className="gradient-text">MARKER FAM</span>
+      </h1>
+
+      <span
+        className="font-lg weight-300 gray mb-md"
+        style={{ textAlign: "center" }}
+      >
+        <div></div>
+      </span>
+    </div>
+  );
+}
